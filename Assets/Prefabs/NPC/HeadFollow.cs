0979a7b0cd479ff2e5a0784d86777a78e4f71cec
Menuoo <lietuvis7080@gmail.com>
2025-11-28 13:08:41 +0200

    nauya is in the game

--- Assets/Prefabs/NPC/HeadFollow.cs
+++ Assets/Prefabs/NPC/HeadFollow.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class HeadFollow : MonoBehaviour
 {
     [SerializeField] GameObject headBone;
+
+    [SerializeField] bool lockX = false;
+    [SerializeField] bool lockY = false;
+    [SerializeField] bool lockZ = false;
+
     PlayerController player;
+    Vector3 eulerRot;
 
     private void Start()
     {
@@ -15,6 +21,13 @@ public class HeadFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        eulerRot = headBone.transform.rotation.eulerAngles;
         headBone.transform.LookAt(player.transform);
+
+        eulerRot = new Vector3(lockX ? eulerRot.x : headBone.transform.rotation.eulerAngles.x,
+                               lockY ? eulerRot.y : headBone.transform.rotation.eulerAngles.y,
+                               lockZ ? eulerRot.z : headBone.transform.rotation.eulerAngles.z);
+
+        headBone.transform.rotation = Quaternion.Euler(eulerRot);
     }
 }
